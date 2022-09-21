1917e35bc897d269950bc455773f1c6c5f3bafde
hha <heinhtetaung@m-techsolutions.io>
2022-09-21 16:24:21 +0630

    first commit

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,63 +1,64 @@
-import { useFrame, useThree } from "@react-three/fiber"
-import { useSphere } from "@react-three/cannon"
-import { useEffect, useRef } from "react"
-import { Vector3 } from "three"
-import { useKeyboard } from "../hooks/useKeyboard"
+import { useFrame, useThree } from '@react-three/fiber'
+import React, { useEffect, useRef } from 'react'
+import { useSphere } from '@react-three/cannon'
+import { Vector3 } from 'three'
+import { useKeyBoard } from '../hooks/useKeyboard'
 
-const JUMP_FORCE = 4;
-const SPEED = 4;
+const Player = () => {
 
-export const Player = () => {
-	const { moveBackward, moveForward, moveRight, moveLeft, jump } = useKeyboard()
+	const {moveBackward,moveForward,moveLeft,moveRight,jump}=useKeyBoard()
 
-	const { camera } = useThree()
-	const [ref, api] = useSphere(() => ({
-		mass: 1,
-		type: 'Dynamic',
-		position: [0, 1, 0],
+
+	const {camera}=useThree()
+	const [ref,api]=useSphere(()=>({
+		mass:1,
+		type:'Dynamic',
+		position:[0,1,0]
 	}))
 
-	const vel = useRef([0, 0, 0])
-	useEffect(() => {
-		api.velocity.subscribe((v) => vel.current = v)
-	}, [api.velocity])
-
-	const pos = useRef([0, 0, 0])
-	useEffect(() => {
-		api.position.subscribe((p) => pos.current = p)
-	}, [api.position])
-
-	useFrame(() => {
-		camera.position.copy(new Vector3(pos.current[0], pos.current[1], pos.current[2]))
-
-		const direction = new Vector3()
-
-		const frontVector = new Vector3(
+	const pos=useRef([0,0,0])
+	const vel=useRef([0,0,0])
+	useFrame(()=>{
+		camera.position.copy(new Vector3(pos.current[0],pos.current[1],pos.current[2]))
+		
+		const direction=new Vector3()
+		const frontVector=new Vector3(
 			0,
 			0,
-			(moveBackward ? 1 : 0) - (moveForward ? 1 : 0)
+			(moveBackward?1:0) -(moveForward?1:0)
 		)
 
-		const sideVector = new Vector3(
-			(moveLeft ? 1 : 0) - (moveRight ? 1 : 0),
+		const sideVector=new Vector3(
+			(moveLeft?1:0) -(moveRight?1:0),
 			0,
-			0,
+			0
 		)
 
 		direction
-			.subVectors(frontVector, sideVector)
-			.normalize()
-			.multiplyScalar(SPEED)
-			.applyEuler(camera.rotation)
+		.subVectors(frontVector,sideVector)
+		.normalize()
+		.multiplyScalar(4)
+		.applyEuler(camera.rotation)
 
-		api.velocity.set(direction.x, vel.current[1], direction.z)
+		api.velocity.set(direction.x,vel.current[1],direction.z)
 
-		if (jump && Math.abs(vel.current[1]) < 0.05) {
-			api.velocity.set(vel.current[0], JUMP_FORCE, vel.current[2])
+		if(jump && Math.abs(vel.current[1])){
+			api.velocity.set(vel.current[0],3,vel.current[1])
 		}
-	})
+		})
+ 
+	useEffect(()=>{
+		api.velocity.subscribe((x)=>vel.current=x)
+	},[api.velocity])
+	useEffect(()=>{
+		api.position.subscribe((x)=>pos.current=x)
+	},[api.position])
 
-	return (
-		<mesh ref={ref}></mesh>
-	)
+  return (
+	<mesh ref={ref}>
+
+	</mesh>
+  )
 }
+
+export default Player
--- src/hooks/useKeyboard.js
+++ src/hooks/useKeyboard.js
@@ -1,67 +1,70 @@
 import { useCallback, useEffect, useState } from "react"
 
-function actionByKey(key) {
-	const keyActionMap = {
-		KeyW: 'moveForward',
-		KeyS: 'moveBackward',
-		KeyA: 'moveLeft',
-		KeyD: 'moveRight',
-		Space: 'jump',
-		Digit1: 'dirt',
-		Digit2: 'grass',
-		Digit3: 'glass',
-		Digit4: 'wood',
-		Digit5: 'log',
+const actionByKey=(key)=>{
+	const keyActionMap={
+		KeyW:'moveForward',
+		KeyS:'moveBackward',
+		KeyA:'moveLeft',
+		KeyD:'moveRight',
+		Space:'jump',
+		Digit1:'dirt',
+		Digit2:'grass',
+		Digit3:'glass',
+		Digit4:'wood',
+		Digit5:'log',
 	}
 	return keyActionMap[key]
 }
 
-export const useKeyboard = () => {
-	const [actions, setActions] = useState({
-		moveForward: false,
-		moveBackward: false,
-		moveLeft: false,
-		moveRight: false,
-		jump: false,
-		dirt: false,
-		grass: false,
-		glass: false,
-		wood: false,
-		log: false,
+export const useKeyBoard=()=>{
+	const [action,setAction]=useState({
+		moveForward:false,
+		moveBackward:false,
+		moveLeft:false,
+		moveRight:false,
+		jump:false,
+		texture1:false,
+		texture2:false,
+		texture3:false,
+		texture4:false,
+		texture5:false,
 	})
 
-	const handleKeyDown = useCallback((e) => {
-		const action = actionByKey(e.code)
-		if (action) {
-			setActions((prev) => {
-				return ({
-					...prev,
-					[action]: true
+	const handleDown=useCallback((e)=>{
+		const action=actionByKey(e.code)
+			if(action){
+				setAction((prev)=>{
+					return {
+						...prev,
+						[action]:true
+					}
 				})
+			}
+		}
+		,[])
+
+	const handleUp=useCallback((e)=>{
+		const action=actionByKey(e.code)
+		if(action){
+			setAction((prev)=>{
+				return {
+					...prev,
+					[action]:false
+				}
 			})
 		}
-	}, [])
+	},[])
 
-	const handleKeyUp = useCallback((e) => {
-		const action = actionByKey(e.code)
-		if (action) {
-			setActions((prev) => {
-				return ({
-					...prev,
-					[action]: false
-				})
-			})
+	useEffect(()=>{
+		document.addEventListener('keydown',handleDown)
+		document.addEventListener('keyup',handleUp)
+		return ()=>{
+			document.removeEventListener('keydown',handleDown)
+		document.removeEventListener('keyup',handleUp)
 		}
-	}, [])
+	},[handleUp,handleDown])
 
-	useEffect(() => {
-		document.addEventListener('keydown', handleKeyDown)
-		document.addEventListener('keyup', handleKeyUp)
-		return () => {
-			document.removeEventListener('keydown', handleKeyDown)
-			document.removeEventListener('keyup', handleKeyUp)
-		}
-	}, [handleKeyDown, handleKeyUp])
+	return action
+	
 
-	return actions
 }
